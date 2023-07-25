262464e0cfd129133a413544cba4ea5815c68236
nico villa <nicovillanova@hotmail.com>
2023-07-25 17:11:39 +0200

    fin con bonus

---
+++ your-code/update.sql
@@ -0,0 +1,15 @@
+UPDATE Salesperson
+SET store = 'Miami'
+WHERE salep_id = 4;
+
+UPDATE Customers
+SET email = 'ppicasso@gmail.com'
+WHERE cust_id = 0;
+
+UPDATE Customers
+SET email = 'lamarr_hd@hollywood.com'
+WHERE cust_id = 1;
+
+UPDATE Customers
+SET email = 'hello@nasa.gov'
+WHERE cust_id = 2;
